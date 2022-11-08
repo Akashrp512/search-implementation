94b6fea1e412760dae585da9c04297340bf9c0a0
Akash RP <akashrp512@gmail.com>
2022-11-08 15:00:57 +0530

    commit

---
+++ src/component/ImageList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImageList = () => {
+  return (
+    <div>
+      Image List
+    </div>
+  )
+}
+
+export default ImageList
---
+++ src/component/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <div>
+      Search bar
+    </div>
+  )
+}
+
+export default SearchBar

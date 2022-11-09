18e4710762c8032cd22072eadf31aab68733225f
Akash RP <akashrp512@gmail.com>
2022-11-09 12:21:28 +0530

    commit

--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -4,10 +4,16 @@ class SearchBar extends React.Component {
 
  state ={term: ''};
 
+ onFormSubmit =(event) => {
+  event.preventDefault();
+
+  console.log(this.state.term);
+ }
+
   render() {
     return (
     <div className='ui segment'>
-    <form className='ui form'>
+    <form onSubmit={this.onFormSubmit} className='ui form'>
     <div className='field'>
     <label>Image Search</label>
       <input 
